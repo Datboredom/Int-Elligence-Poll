dee939dd9f78f98f00871d3b8a0263e51ea9705a
ACJ123 <flameaj@gmail.com>
2019-02-19 16:57:47 -0500

    added reset
    added the back end and front end for reset button based off the original 4 buttons

---
+++ updated_viewer.js
@@ -0,0 +1,65 @@
+var firstclicks = 0;
+function onFirstClick() {
+    firstclicks += 1;
+    document.getElementById("First").innerHTML = firstclicks;
+    document.getElementById("formFieldSet").disabled = true;
+    CheckAnswer();
+};
+var secondclicks = 0;
+function onSecondClick() {
+    secondclicks += 1;
+    document.getElementById("Second").innerHTML = secondclicks;
+    document.getElementById("formFieldSet").disabled = true;
+    CheckAnswer();
+};
+
+var thirdclicks = 0;
+function onThirdClick() {
+    thirdclicks += 1;
+    document.getElementById("Third").innerHTML = thirdclicks;
+    document.getElementById("formFieldSet").disabled = true;
+    CheckAnswer();
+};
+var fourthclicks = 0;
+function onFourthClick() {
+    fourthclicks += 1;
+    document.getElementById("Fourth").innerHTML = fourthclicks;
+    document.getElementById("formFieldSet").disabled = true;
+    CheckAnswer();
+};
+var resetClick = 0;
+function onResetClick() {
+    resetClick += 1;
+    firstclicks = 0;
+    secondclicks = 0;
+    thirdclicks = 0;
+    fourthclicks = 0;
+    document.getElementById("Reset Poll").innerHTML = resetClick;
+    document.getElementById("First").innerHTML = firstclicks;
+    document.getElementById("Second").innerHTML = secondclicks;
+    document.getElementById("Third").innerHTML = thirdclicks;
+    document.getElementById("Fourth").innerHTML = fourthclicks;
+    document.getElementById("formFieldSet").disabled = true;
+    CheckAnswer();
+};
+function CheckAnswer() {
+    var Answer = "";
+    if (Math.max(firstclicks, secondclicks, thirdclicks, fourthclicks, resetClick) == firstclicks) {
+        Answer = "Medieval";
+    }
+    if (Math.max(firstclicks, secondclicks, thirdclicks, fourthclicks, resetClick) == secondclicks) {
+        Answer = "Futuristic";
+    }
+    if (Math.max(firstclicks, secondclicks, thirdclicks, fourthclicks, resetClick) == thirdclicks) {
+        Answer = "Apocalyptic";
+    }
+    if (Math.max(firstclicks, secondclicks, thirdclicks, fourthclicks, resetClick) == fourthclicks) {
+        Answer = "Modern";
+    }
+    if (Math.max(firstclicks, secondclicks, thirdclicks, fourthclicks, resetClick) == resetClick) {
+      Answer = "Reset Poll";
+    }
+    document.getElementById("Answer").innerHTML = Answer;
+}
+function FindMax() {
+}
