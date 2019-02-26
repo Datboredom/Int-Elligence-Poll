68321183c675c4f96df1fba625867dc891f47449
ACJ123 <flameaj@gmail.com>
2019-02-26 14:30:50 -0500

    finalized reset

--- updated_viewer.js
+++ updated_viewer.js
@@ -1,3 +1,89 @@
+var token = "";
+var tuid = "";
+var ebs = "";
+
+// because who wants to type this every time?
+var twitch = window.Twitch.ext;
+
+// create the request options for our Twitch API calls
+var requests = {
+    set: createRequest('POST', 'retrieveAns'),
+    get: createRequest('GET', 'sendAns')
+};
+
+function createRequest(type, method) {
+
+    return {
+        type: type,
+        url: location.protocol + '//localhost:5000/' + method,
+        success: updateBlock,
+        error: logError
+    }
+}
+
+function setAuth(token) {
+    Object.keys(requests).forEach((req) => {
+        twitch.rig.log('Setting auth headers');
+        requests[req].headers = { 'Authorization': 'Bearer ' + token }
+    });
+}
+
+twitch.onContext(function (context) {
+    twitch.rig.log(context);
+});
+
+twitch.onAuthorized(function (auth) {
+    // save our credentials
+    token = auth.token;
+    tuid = auth.userId;
+
+    // enable the button
+    //$('#cycle').removeAttr('disabled');
+
+    setAuth(token);
+    $.ajax(requests.get);
+});
+
+function updateBlock() {
+    twitch.rig.log('i think its working');
+    //$('#color').css('background-color', hex);
+}
+
+function logError(_, error, status) {
+    twitch.rig.log('EBS request returned ' + status + ' (' + error + ')');
+}
+
+function logSuccess(status) {
+    // we could also use the output to update the block synchronously here,
+    // but we want all views to get the same broadcast response at the same time.
+    twitch.rig.log('EBS request returned ' + ' (' + status + ')');
+}
+
+$(function () {
+
+    // CHECK HERE (.data)
+    $('#retrieveAns').data(function () {
+        if (!token) { return twitch.rig.log('Not authorized'); }
+        twitch.rig.log('Requesting an answer');
+        $.ajax(requests.set);
+    });
+
+    // listen for incoming broadcast message from our EBS
+    twitch.listen('broadcast', function (target, contentType) {
+        twitch.rig.log('Received broadcast answers');
+        updateBlock();
+    });
+});
+
+function toggleShowHide() {
+    var form = document.getElementById("formFieldSet");
+    if (form.style.display === "none") {
+        form.style.display = "block";
+    } else {
+        form.style.display = "none";
+    }
+}
+
 var firstclicks = 0;
 function onFirstClick() {
     firstclicks += 1;
@@ -27,36 +113,63 @@ function onFourthClick() {
     document.getElementById("formFieldSet").disabled = true;
     CheckAnswer();
 };
-//var resetClick = 0;
+
+var resetClick = 0;
 function onResetClick() {
-    document.getElementById("formFieldSet").disabled = false;
-    //resetClick += 1;
+    resetClick += 1;
     firstclicks = 0;
     secondclicks = 0;
     thirdclicks = 0;
     fourthclicks = 0;
-   // document.getElementById("Reset_Poll").innerHTML = resetClick;
+    document.getElementById("Reset Poll").innerHTML = resetClick;
     document.getElementById("First").innerHTML = firstclicks;
     document.getElementById("Second").innerHTML = secondclicks;
     document.getElementById("Third").innerHTML = thirdclicks;
     document.getElementById("Fourth").innerHTML = fourthclicks;
+    document.getElementById("formFieldSet").disabled = true;
     CheckAnswer();
 };
+
+function onResetClick() {
+    var form = document.getElementById("formFieldSet");
+    document.getElementById("formFieldSet").disabled = false;
+    firstclicks = 0;
+    secondclicks = 0;
+    thirdclicks = 0;
+    fourthclicks = 0;
+    document.getElementById("First").innerHTML = firstclicks;
+    document.getElementById("Second").innerHTML = secondclicks;
+    document.getElementById("Third").innerHTML = thirdclicks;
+    document.getElementById("Fourth").innerHTML = fourthclicks;
+    form.style.display = "block"
+    CheckAnswer();
+};
+
 function CheckAnswer() {
     var Answer = "";
     if (Math.max(firstclicks, secondclicks, thirdclicks, fourthclicks) == firstclicks) {
         Answer = "Medieval";
+        if (firstclicks == 0){
+                    Answer = " ";
+                }
     }
     if (Math.max(firstclicks, secondclicks, thirdclicks, fourthclicks) == secondclicks) {
         Answer = "Futuristic";
+        if (secondclicks == 0){
+                    Answer = " ";
+                }
     }
     if (Math.max(firstclicks, secondclicks, thirdclicks, fourthclicks) == thirdclicks) {
         Answer = "Apocalyptic";
+        if (thirdclicks == 0){
+                    Answer = " ";
+                }
     }
     if (Math.max(firstclicks, secondclicks, thirdclicks, fourthclicks) == fourthclicks) {
         Answer = "Modern";
+        if (fourthclicks == 0){
+            Answer = " ";
+        }
     }
     document.getElementById("Answer").innerHTML = Answer;
-}
-function FindMax() {
 }
